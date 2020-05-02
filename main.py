ca9ffa302b0ceee14a01079cf88362d92aeb411d
Kalmanis <Kalmanis@github.com>
2020-05-02 13:11:55 +0300

    a

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request
 
-
-app = Flask('app')
+app = Flask(__name__)
 
 
 @app.route('/')
-def index_page():
+def home():
   return render_template('BioM.html')
 
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+app.run(host='0.0.0.0', port=8020)
